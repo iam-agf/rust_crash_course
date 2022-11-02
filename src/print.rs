faa1f0144b36470c1962074236f28fbba6d1b10d
iamagf <agf0710@gmail.com>
2022-11-02 17:48:45 -0600

    Run fn: printing traits

--- src/print.rs
+++ src/print.rs
@@ -1,5 +1,18 @@
 pub fn run() {
+    // Print to console
     println!("Hello from the print file!");
 
-    println!("Number: {}", 1)
+    // Basic Formatting
+    println!("{} is from {}", "Brad", "Mass");
+
+    // Positional parameters
+    println!("{0} is from {1} and {2} likes to {1}", "Brad", "Mass", "Code",);
+
+    // Named arguments
+    println!("{name} lines to play {activity}",
+    name = "Brad",
+    activity = "Baseball");
+
+    // Placeholder traits
+    println!("Binary: {:b}\nHex: {:x}\nOcto: {:o}",10,10,10)
 }
